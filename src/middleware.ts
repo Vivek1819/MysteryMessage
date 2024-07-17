5af9d663191a8f3cd55f56beb4fe46c2fbe03321
Vivek Hipparkar <vivekhipparkar@gmail.com>
2024-07-17 22:37:12 +0530

    signin redirect for no token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest } from 'next/server'
 export {default} from "next-auth/middleware"
 import { getToken } from 'next-auth/jwt'
  
@@ -18,7 +18,11 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL('/dashboard', request.url)) 
     }
 
-    //return NextResponse.redirect(new URL('/home', request.url))
+    if(!token && url.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/sign-in', request.url)) 
+    }
+
+    return NextResponse.next();
 }
  
 export const config = {
